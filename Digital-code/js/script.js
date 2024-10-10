5726055885e61b248b3fcc4a24bdf01f5f3ffad4
taithedev2003@gmail.com <99863150+tai2319@users.noreply.github.com>
2024-10-10 09:23:35 +0700

    full

--- Digital-code/js/script.js
+++ Digital-code/js/script.js
@@ -83,6 +83,120 @@ function open_img(evt, cityName) {
   evt.currentTarget.classList.add("active");
 }
 
+//responsive team slider
+
+$(".team-slider").slick({
+  arrows: false,
+  dots: true,
+  infinite: false,
+  autoplay: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll1: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll1: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll1: 1,
+      },
+    },
+  ],
+});
+
+//FAQ section
+document.addEventListener("DOMContentLoaded", function () {
+  let accordionButtons = document.querySelectorAll(".accordion-button");
+  let accimg = document.querySelectorAll(".accordion-button img");
+
+  accordionButtons.forEach(function (button, index) {
+    button.addEventListener("click", function () {
+      let collapse = this.parentElement.nextElementSibling;
+
+      //close all  the other accordion item
+
+      accordionButtons.forEach(function (otherButton, otherIndex) {
+        if (otherButton != button) {
+          let otherCollapse = otherButton.parentElement.nextElementSibling;
+          otherCollapse.style.maxHeight = null;
+
+          //reset the image source and rotaiton for other  accordion
+
+          accimg[otherIndex].src = "./Images/icon/plus.png";
+          accimg[otherIndex].style.transform = "rotate(0deg )";
+          otherButton.style.backgroundColor = "#fff";
+        }
+      });
+      //toggle the clicked according item
+      if (collapse.style.maxHeight) {
+        collapse.style.maxHeight = null;
+
+        accimg[index].src = "./Images/icon/plus.png";
+        accimg[index].style.transform = "rotate(90deg)";
+        button.style.backgroundColor = "";
+      } else {
+        collapse.style.maxHeight = collapse.scrollHeight + "px";
+
+        accimg[index].src = "./Images/icon/menus.png";
+        accimg[index].style.transform = "rotate(180deg)";
+        button.style.backgroundColor = "#c1b0b5";
+      }
+    });
+  });
+});
+
+//footer validate
+
+const fom = document.getElementById("footer-form");
+const footerMessage = document.getElementById("footer-message");
+
+fom.addEventListener("submit", (event) => {
+  event.preventDefault();
+  footerMessage.innerHTML = "~ Form sumbited successfully";
+  footerMessage.style.display = "flex";
+  fom.reset();
+
+  setTimeout(() => {
+    footerMessage.style.display = "none";
+  }, 3000);
+});
+
+// button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("backToTopBtn").style.display = "block";
+  } else {
+    document.getElementById("backToTopBtn").style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  const scrollTop = document.documentElement || document.body;
+  scrollTop.scrollIntoView({
+    bahavior: "smooth",
+  });
+}
+
 // responsive logoipsum slider
 
 $(".slider-logo").slick({
@@ -92,13 +206,13 @@ $(".slider-logo").slick({
   autoplay: false,
   speed: 300,
   slidesToShow: 5,
-  sliderToScroll: 1,
+  slidesToScroll: 1,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
         slidesToShow: 4,
-        sliderToScroll: 1,
+        slidesToScroll: 1,
         infinite: true,
         dots: false,
       },
@@ -107,7 +221,7 @@ $(".slider-logo").slick({
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
-        sliderToScroll1: 1,
+        slidesToScroll1: 1,
       },
     },
   ],
